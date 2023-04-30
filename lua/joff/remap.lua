b7838cf1dcb18ae7b748e9d3f0f458fe208c6d93
Joffrey Lagut <j.lagut@live.fr>
2023-04-30 18:43:10 +0200

    Add jk and kj to ESC interactive mode

--- lua/joff/remap.lua
+++ lua/joff/remap.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = True })
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = True })
