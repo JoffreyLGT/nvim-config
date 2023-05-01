0a722c9afb0c4cf63d06b2503dc226fe3d1a4cc9
Joffrey Lagut <j.lagut@live.fr>
2023-05-01 09:09:20 +0200

    Remap keymaps for easy split movements

--- lua/joff/remap.lua
+++ lua/joff/remap.lua
@@ -1,3 +1,5 @@
+local opts = { noremap = True }
+
 vim.g.mapleader = " "
 
 -- Display project view (files)
@@ -8,8 +10,14 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- To get out of insert mode without pressing Escape
-vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = True })
-vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = True })
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", opts)
+
+-- Easier movement between split
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Nvim-tree
-vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<cr>", opts)
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<cr>")
